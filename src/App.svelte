ba689ad14079d6f6bb5aa1e325baeaa9a92f8a8d
kurozenzen <kurozenzen@gmail.com>
2022-08-31 21:41:54 +0200

    chore: format app

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  import Header from "./lib/navigation/Header.svelte";
-  import currentPage from "./lib/navigation/currentPage";
-  import Search from "./lib/search/Search.svelte";
-  import Preferences from "./lib/preferences/Preferences.svelte";
-  import Help from "./lib/help/Help.svelte";
-  import Account from "./lib/account/Account.svelte";
-  import InjectTheme from "./lib/theme/InjectTheme.svelte";
+  import Header from './lib/navigation/Header.svelte'
+  import currentPage from './lib/navigation/currentPage'
+  import Search from './lib/search/Search.svelte'
+  import Preferences from './lib/preferences/Preferences.svelte'
+  import Help from './lib/help/Help.svelte'
+  import Account from './lib/account/Account.svelte'
+  import InjectTheme from './lib/theme/InjectTheme.svelte'
 </script>
 
 <InjectTheme />
@@ -13,13 +13,13 @@
 <Header />
 
 <main>
-  {#if $currentPage === "search"}
+  {#if $currentPage === 'search'}
     <Search />
-  {:else if $currentPage === "account"}
+  {:else if $currentPage === 'account'}
     <Account />
-  {:else if $currentPage === "settings"}
+  {:else if $currentPage === 'settings'}
     <Preferences />
-  {:else if $currentPage === "help"}
+  {:else if $currentPage === 'help'}
     <Help />
   {:else}
     <p>You got lost...</p>
