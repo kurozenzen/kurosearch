54874e2f0db89cffd00ce616ed013b4698faecca
kurozenzen <kurozenzen@gmail.com>
2022-09-11 22:39:28 +0200

    feat: extract routing logic into its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,9 @@
 <script>
   import Header from './lib/navigation/Header.svelte'
-  import currentPage from './lib/navigation/currentPage'
-  import Search from './lib/search/Search.svelte'
-  import Preferences from './lib/preferences/Preferences.svelte'
-  import Help from './lib/help/Help.svelte'
-  import Account from './lib/account/Account.svelte'
   import InjectTheme from './lib/theme/InjectTheme.svelte'
+  import Router from './lib/router/Router.svelte'
 </script>
 
 <InjectTheme />
-
 <Header />
-
-<main>
-  {#if $currentPage === 'search'}
-    <Search />
-  {:else if $currentPage === 'account'}
-    <Account />
-  {:else if $currentPage === 'settings'}
-    <Preferences />
-  {:else if $currentPage === 'help'}
-    <Help />
-  {:else}
-    <p>You got lost...</p>
-  {/if}
-</main>
+<Router />
---
+++ src/lib/router/Router.svelte
@@ -0,0 +1,21 @@
+<script>
+  import currentPage from '../navigation/currentPage'
+  import Search from '../search/Search.svelte'
+  import Preferences from '../preferences/Preferences.svelte'
+  import Help from '../help/Help.svelte'
+  import Account from '../account/Account.svelte'
+</script>
+
+<main>
+  {#if $currentPage === 'search'}
+    <Search />
+  {:else if $currentPage === 'account'}
+    <Account />
+  {:else if $currentPage === 'settings'}
+    <Preferences />
+  {:else if $currentPage === 'help'}
+    <Help />
+  {:else}
+    <p>You got lost...</p>
+  {/if}
+</main>
