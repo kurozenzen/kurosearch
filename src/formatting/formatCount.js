c1476a9662336308627c6ea6793e74d1312b5e5b
kurozenzen <kurozenzen@gmail.com>
2022-09-11 22:38:31 +0200

    feat: simplify count formatting

--- src/formatting/formatCount.js
+++ src/formatting/formatCount.js
@@ -1,5 +1,7 @@
 import { isValidCount } from '../tags/validation'
 
+const formatter = Intl.NumberFormat('en', { notation: 'compact' })
+
 /**
  * @param {number} value
  */
@@ -7,14 +9,6 @@ export const formatCount = (value) => {
   if (!isValidCount(value)) {
     throw new TypeError('Invalid value passed to formatCount')
   }
-
-  if (value >= 1e6) {
-    return `${Math.trunc(value / 1e5) / 10}M`
-  }
-
-  if (value >= 1e3) {
-    return `${Math.trunc(value / 1e2) / 10}K`
-  }
-
-  return value.toString()
+  
+  return formatter.format(value)
 }
