7485bc5304d2b9dad8cede916189497b8686cdd8
kurozenzen <kurozenzen@gmail.com>
2023-01-05 00:00:34 +0100

    fix: render backend down

--- src/lib/preferences/currentBackend.js
+++ src/lib/preferences/currentBackend.js
@@ -1,17 +1,11 @@
-let currentBackend = 'render'
+let currentBackend = 'adaptable'
 
 const BACKEND_URLS = Object.freeze({
-  heroku: 'https://r34-json.herokuapp.com/v2',
   adaptable: 'https://rule34-json-api.adaptable.app/v2',
-  render: 'https://json-api.onrender.com/v2',
-  railway: 'https://api-service-production-122b.up.railway.app/v2',
 })
 
 export const BACKENED_NAMES = Object.freeze({
-  heroku: 'Heroku',
   adaptable: 'Adaptable',
-  render: 'Render',
-  railway: 'Railway',
 })
 
 export const getCurrentBackendBaseUrl = () => BACKEND_URLS[currentBackend]
