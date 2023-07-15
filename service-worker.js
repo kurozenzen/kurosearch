e183912d57df449261e4b6a33c29e7c8bc924b45
kurozenzen <44543171+kurozenzen@users.noreply.github.com>
2023-07-15 19:07:03 +0000

    Deploying to gh-pages from @ kurozenzen/kurosearch@1deb4a6bbf45506c1bf7b6ef714dc78957fc8923 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), n = [
-  s + "/app/immutable/entry/app.aef80c86.js",
-  s + "/app/immutable/assets/0.077dd1be.css",
-  s + "/app/immutable/nodes/0.38cbcb71.js",
-  s + "/app/immutable/nodes/1.0f2b79d5.js",
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
+  s + "/app/immutable/entry/app.f9a9f67c.js",
+  s + "/app/immutable/assets/0.d7a2e476.css",
+  s + "/app/immutable/nodes/0.9beb6ddd.js",
+  s + "/app/immutable/nodes/1.5065c903.js",
   s + "/app/immutable/assets/2.1cf4e4db.css",
-  s + "/app/immutable/nodes/2.a2bc5c8b.js",
+  s + "/app/immutable/nodes/2.d455e1e9.js",
   s + "/app/immutable/assets/3.4d4e147c.css",
-  s + "/app/immutable/nodes/3.2b50323f.js",
+  s + "/app/immutable/nodes/3.c00cee1a.js",
   s + "/app/immutable/assets/4.62d0a4b1.css",
   s + "/app/immutable/nodes/4.82aaf874.js",
   s + "/app/immutable/assets/5.c908c19d.css",
-  s + "/app/immutable/nodes/5.9dd3e433.js",
+  s + "/app/immutable/nodes/5.6f4c0c50.js",
   s + "/app/immutable/assets/6.eab0bbcc.css",
   s + "/app/immutable/nodes/6.19caa5c6.js",
   s + "/app/immutable/assets/7.57c90ca4.css",
-  s + "/app/immutable/nodes/7.f170799e.js",
-  s + "/app/immutable/nodes/8.cb77d169.js",
+  s + "/app/immutable/nodes/7.fc5735fc.js",
+  s + "/app/immutable/nodes/8.c7af4d86.js",
   s + "/app/immutable/assets/9.2860deb1.css",
-  s + "/app/immutable/nodes/9.1047b382.js",
+  s + "/app/immutable/nodes/9.26facb79.js",
   s + "/app/immutable/assets/Checkbox.de4b02c1.css",
   s + "/app/immutable/assets/CodiconLink.dd18c308.css",
   s + "/app/immutable/assets/DetailedTag.8dfd7a41.css",
@@ -25,8 +25,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), n
   s + "/app/immutable/assets/Dialog.5cd67abb.css",
   s + "/app/immutable/assets/Heading1.fb3043f7.css",
   s + "/app/immutable/assets/LoadingAnimation.79e4bc51.css",
-  s + "/app/immutable/assets/ModifierSelect.eea8b3a6.css",
-  s + "/app/immutable/assets/RotatingTextSelect.7cae4648.css",
+  s + "/app/immutable/assets/ModifierSelect.31c7e0b5.css",
   s + "/app/immutable/assets/ScrollUpButton.f47fbc2e.css",
   s + "/app/immutable/assets/Select.a851e2ac.css",
   s + "/app/immutable/assets/SettingsLink.41ba7049.css",
@@ -36,15 +35,15 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), n
   s + "/app/immutable/chunks/Checkbox.d309d84b.js",
   s + "/app/immutable/chunks/CodiconLink.289bed27.js",
   s + "/app/immutable/chunks/DetailedTag.19f75f3d.js",
-  s + "/app/immutable/chunks/Details.3a2c6f0c.js",
+  s + "/app/immutable/chunks/Details.ac67f445.js",
   s + "/app/immutable/chunks/Dialog.249a2b75.js",
   s + "/app/immutable/chunks/Heading1.a729be81.js",
   s + "/app/immutable/chunks/LoadingAnimation.4f862ba3.js",
-  s + "/app/immutable/chunks/ModifierSelect.e69c9799.js",
-  s + "/app/immutable/chunks/RotatingTextSelect.5aa910cd.js",
+  s + "/app/immutable/chunks/ModifierSelect.2537b61e.js",
+  s + "/app/immutable/chunks/RotatingTextSelect.d2177178.js",
   s + "/app/immutable/chunks/ScrollUpButton.c06faa1c.js",
   s + "/app/immutable/chunks/Select.307673e1.js",
-  s + "/app/immutable/chunks/SettingsLink.8ca74f82.js",
+  s + "/app/immutable/chunks/SettingsLink.d416799f.js",
   s + "/app/immutable/chunks/SimpleTag.12ca1edf.js",
   s + "/app/immutable/chunks/TextButton.53e352d9.js",
   s + "/app/immutable/chunks/TextInput.32d03d64.js",
@@ -57,20 +56,20 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), n
   s + "/app/immutable/chunks/index.esm2017.edf8478c.js",
   s + "/app/immutable/chunks/localstorage-enabled-store.3ef89c8f.js",
   s + "/app/immutable/assets/localstorage-enabled-store.2fafd00f.css",
-  s + "/app/immutable/chunks/paths.34259c5d.js",
+  s + "/app/immutable/chunks/paths.5eb03071.js",
   s + "/app/immutable/chunks/scheduler.6c5d9662.js",
   s + "/app/immutable/chunks/search-builder.006009c0.js",
   s + "/app/immutable/assets/search-builder.f7cf8fcb.css",
-  s + "/app/immutable/chunks/singletons.1ebf5b30.js",
+  s + "/app/immutable/chunks/singletons.9a83742e.js",
   s + "/app/immutable/chunks/store-keys.e70c3dff.js",
-  s + "/app/immutable/chunks/supertags-store.b45ef135.js",
+  s + "/app/immutable/chunks/supertags-store.3af9ff87.js",
   s + "/app/immutable/assets/supertags-store.993f1438.css",
   s + "/app/immutable/chunks/tag-modifier-data.021c0243.js",
   s + "/app/immutable/chunks/tag-type-data.934b5280.js",
   s + "/app/immutable/chunks/tag-utils.ceb6413c.js",
   s + "/app/immutable/chunks/tags.afa77e19.js",
   s + "/app/immutable/chunks/theme-store.0f902d3d.js",
-  s + "/app/immutable/entry/start.0b43f5d6.js"
+  s + "/app/immutable/entry/start.b744b764.js"
 ], u = [
   s + "/.nojekyll",
   s + "/assets/default-user.png",
@@ -101,15 +100,15 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), n
   s + "/icon/ks-maskable-96.png",
   s + "/manifest.json",
   s + "/robots.txt"
-], l = "1689380357068", i = `cache-${l}`, m = [
-  ...n,
+], l = "1689447998495", i = `cache-${l}`, n = [
+  ...m,
   // the app itself
   ...u.filter((a) => !a.endsWith(".nojekyll"))
   // everything in `static`
 ];
 self.addEventListener("install", (a) => {
   async function t() {
-    await (await caches.open(i)).addAll(m);
+    await (await caches.open(i)).addAll(n);
   }
   a.waitUntil(t());
 });
@@ -125,7 +124,7 @@ self.addEventListener("fetch", (a) => {
     return;
   async function t() {
     const e = new URL(a.request.url), c = await caches.open(i);
-    if (m.includes(e.pathname))
+    if (n.includes(e.pathname))
       return c.match(e.pathname);
     try {
       const p = await fetch(a.request);
