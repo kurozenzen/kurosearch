d851cb305f11184d5f045f16bf7de00ae945421f
kurozenzen <kurozenzen@gmail.com>
2023-07-16 02:35:14 +0200

    feat: changes for kurosearch.com

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/kurosearch-static-experiment' : ''
+			base: ''
 		},
 		appDir: 'app'
 	}
