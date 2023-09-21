31c478231946634cebe480e949b8d8695230b6d0
kurozenzen <kurozenzen@gmail.com>
2023-09-21 20:09:59 +0200

    fix: sharing bug

--- src/lib/logic/share-utils.ts
+++ src/lib/logic/share-utils.ts
@@ -1,14 +1,18 @@
+import { serializeTagName } from './tag-serialization';
 import { createSearchableTag } from './tag-utils';
 
 const DELIMITER = ';';
 
-export const parseShareTags = (value: string): kurosearch.SearchableTag[] =>
-	value
+export const parseShareTags = (value: string): kurosearch.SearchableTag[] => {
+	debugger;
+	return value
+		.replaceAll(' ', '+')
 		.split(DELIMITER)
 		.map((x) => x.trim())
 		.map((group) =>
 			createSearchableTag(group.charAt(0) as kurosearch.TagModifier, group.substring(1))
 		);
+};
 
 export const serializeShareTags = (tags: kurosearch.SearchableTag[]): string =>
-	tags.map((t) => `${t.modifier}${t.name}`).join(DELIMITER);
+	tags.map((t) => `${t.modifier}${serializeTagName(t.name)}`).join(DELIMITER);
