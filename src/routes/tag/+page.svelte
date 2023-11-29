87636b75ebb5ab3d0b3928a2d7a09acb6481be82
kurozenzen <kurozenzen@gmail.com>
2023-11-29 18:24:38 +0100

    feat: add tag page

---
+++ src/routes/tag/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import Searchbar from '$lib/components/kurosearch/searchbar/Searchbar.svelte';
+	import LoadingAnimation from '$lib/components/pure/loading-animation/LoadingAnimation.svelte';
+	import { getTagSuggestions } from '$lib/logic/api-client/ApiClient';
+
+	const name = browser ? new URL(document.location.href).searchParams.get('name') : undefined;
+
+	console.log(name);
+
+	const loadTag = async (name: string) => {
+		const response = await fetch(
+			`https://rule34.xxx/index.php?page=tags&s=list&tags=${name}&sort=asc&order_by=index_count`
+		);
+		const text = await response.text();
+		const parser = new DOMParser();
+		const xml = parser.parseFromString(text, 'text/xml');
+		const tds = xml.querySelectorAll('#content tr td').values();
+
+		return JSON.stringify(tds);
+	};
+</script>
+
+{#if name}
+	{#await loadTag(name)}
+		<LoadingAnimation />
+	{:then data}
+		<p>{data}</p>
+	{/await}
+{:else}
+	<h1>Tags</h1>
+	<Searchbar
+		placeholder="Search for a tag..."
+		fetchSuggestions={getTagSuggestions}
+		on:pick={(event) => {
+			window.location.href = '/tag?name=' + event.detail.label;
+		}}
+	/>
+{/if}
