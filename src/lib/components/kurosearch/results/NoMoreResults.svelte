f59ac4a56be6a3336ca36a1e4133ac46ea926580
kurozenzen <kurozenzen@gmail.com>
2024-02-18 20:27:05 +0100

    feat: new not found image

--- src/lib/components/kurosearch/results/NoMoreResults.svelte
+++ src/lib/components/kurosearch/results/NoMoreResults.svelte
@@ -1,14 +1,70 @@
 <script>
 	import { base } from '$app/paths';
+	import TextButton from '$lib/components/pure/text-button/TextButton.svelte';
+	import activeSupertagsStore from '$lib/store/active-supertags-store';
+	import activeTagsStore from '$lib/store/active-tags-store';
 </script>
 
-<img src="{base}/assets/shironeko-x.png" alt="No more results to load" />
-<p>You have seen everything there is to see...</p>
+<section>
+	<img
+		src="{base}/assets/lynxy-chibi-not-found.webp"
+		alt="No more results to load"
+		width="1231"
+		height="864"
+	/>
+
+	<p>You have seen everything there is to see...</p>
+	<div>
+		<TextButton
+			type="secondary"
+			title="Scroll back up."
+			on:click={() => {
+				window.scrollTo({ top: 0 });
+			}}
+		>
+			Back to Top
+		</TextButton>
+		<TextButton
+			type="secondary"
+			title="Reset tags."
+			on:click={() => {
+				activeTagsStore.reset();
+				activeSupertagsStore.reset();
+			}}
+		>
+			Reset Tags
+		</TextButton>
+	</div>
+</section>
 
 <style>
+	section {
+		overflow: hidden;
+		background-color: var(--background-1);
+		max-width: 400px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--grid-gap);
+	}
+
+	@media (min-width: 400px) {
+		section {
+			border-radius: var(--border-radius-large);
+		}
+	}
+
 	img {
 		display: block;
-		aspect-ratio: 257 / 244;
-		width: 257px;
+		object-fit: cover;
+		aspect-ratio: 1231 / 864;
+		max-width: 100%;
+		max-height: 300px;
+	}
+
+	div {
+		display: flex;
+		gap: var(--grid-gap);
+		padding-block-end: var(--grid-gap);
 	}
 </style>
--- src/lib/components/kurosearch/results/ZeroResults.svelte
+++ src/lib/components/kurosearch/results/ZeroResults.svelte
@@ -10,39 +10,62 @@
 	const dispatch = createEventDispatcher();
 </script>
 
-<img src="{base}/assets/shironeko-confused.png" alt="Nothing here..." />
-<p>We couldn't find any posts matching your tags. Sorry...</p>
-<div>
-	<TextButton
-		title="In case your filter caused all posts to vanish. Reset it."
-		on:click={() => {
-			sort.reset();
-			filter.reset();
-			dispatch('sortfilterupdate');
-		}}
-	>
-		Reset Filter
-	</TextButton>
-	<TextButton
-		title="Tag combinations that are too specific will not yield results."
-		on:click={() => {
-			activeTagsStore.reset();
-			activeSupertagsStore.reset();
-		}}
-	>
-		Reset Tags
-	</TextButton>
-</div>
+<section>
+	<img
+		src="{base}/assets/lynxy-chibi-not-found.webp"
+		alt="Nothing here..."
+		width="1231"
+		height="864"
+	/>
+
+	<p>We couldn't find any posts matching your tags. Sorry...</p>
+	<div>
+		<TextButton
+			type="secondary"
+			title="In case your filter caused all posts to vanish. Reset it."
+			on:click={() => {
+				sort.reset();
+				filter.reset();
+				dispatch('sortfilterupdate');
+			}}
+		>
+			Reset Filter
+		</TextButton>
+		<TextButton
+			type="secondary"
+			title="Tag combinations that are too specific will not yield results."
+			on:click={() => {
+				activeTagsStore.reset();
+				activeSupertagsStore.reset();
+			}}
+		>
+			Reset Tags
+		</TextButton>
+	</div>
+</section>
 
 <style>
+	section {
+		overflow: hidden;
+		background-color: var(--background-1);
+		max-width: 400px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--grid-gap);
+		border-radius: var(--border-radius-large);
+	}
 	img {
 		display: block;
-		aspect-ratio: 272 / 238;
-		width: 272px;
+		object-fit: cover;
+		aspect-ratio: 1231 / 864;
+		max-width: 100%;
+		max-height: 300px;
 	}
 
 	div {
 		display: flex;
 		gap: var(--grid-gap);
+		padding-block-end: var(--grid-gap);
 	}
 </style>
