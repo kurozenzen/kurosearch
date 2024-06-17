635a3d391b5150c1bb24b37f203d3e9c08a1dffd
luimu64 <luimu@luimu.dev>
2024-06-17 17:21:05 +0300

    gifs fixed for the new system

--- src/lib/logic/post-observer.ts
+++ src/lib/logic/post-observer.ts
@@ -6,11 +6,18 @@ export const postObserver = browser
 	? new IntersectionObserver(
 		(entries) => {
 			for (const entry of entries) {
-				const lowSrc = entry.target.getAttribute('data-src-lowres') ?? '';
-				const highSrc = entry.target.getAttribute('data-src-hires') ?? '';
-
+				//check if post is visible
 				if (entry.isIntersecting) {
+					//fetch source urls saved in the image data attributes
+					const lowSrc = entry.target.getAttribute('data-src-lowres') ?? '';
+					const highSrc = entry.target.getAttribute('data-src-hires') ?? '';
+
+					//set post.preview_url to image sources or sources.static to gifs
 					entry?.target?.setAttribute('src', lowSrc);
+
+					//data-src-hires only exists on static images so skip the rest of the function on gifs
+					if (highSrc === '') return;
+					//create new non-visible image and swap the sources once loaded
 					const fullImage = new Image();
 					fullImage.src = highSrc;
 					fullImage.onload = () => {
