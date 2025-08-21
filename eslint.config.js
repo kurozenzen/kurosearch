25f1a63ba42a34047355c38c62a44ffc7130bbd6
kurozenzen <kurozenzen@gmail.com>
2025-08-21 22:59:26 +0200

    feat: Update prettier

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+	js.configs.recommended,
+	{
+		files: ['**/*.{js,ts,mjs}'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 2022
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2017
+			}
+		},
+		plugins: {
+			'@typescript-eslint': ts
+		},
+		rules: {
+			...ts.configs.recommended.rules
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsParser,
+				sourceType: 'module',
+				ecmaVersion: 2022,
+				extraFileExtensions: ['.svelte']
+			},
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		},
+		plugins: {
+			svelte,
+			'@typescript-eslint': ts
+		},
+		rules: {
+			...svelte.configs.recommended.rules,
+			...ts.configs.recommended.rules
+		}
+	},
+	prettier,
+	{
+		ignores: ['build/', '.svelte-kit/', 'dist/', 'node_modules/', '*.d.ts']
+	}
+];
