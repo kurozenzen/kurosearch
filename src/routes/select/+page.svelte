9700c3285eb769d3704c1e91f6dda21b3d44cc2e
kurozenzen <kurozenzen@gmail.com>
2026-05-08 15:57:13 +0200

    feat: Add Quick Search Page

---
+++ src/routes/select/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import KurosearchTitle from '$lib/components/kurosearch/kurosearch-title/KurosearchTitle.svelte';
+	import QuickSearch from '$lib/components/kurosearch/quick-search/QuickSearch.svelte';
+	import LynxMain from '../LynxMain.svelte';
+</script>
+
+<svelte:head>
+	<title>kurosearch - Rule34 Hentai</title>
+	<meta
+		name="description"
+		content="Simple and powerful Rule34/Hentai browsing site with a focus on simplicity and user experience. Supports excluding tags, sorting and filtering."
+	/>
+</svelte:head>
+
+<LynxMain />
+
+
+<KurosearchTitle />
+
+<h2>What are you looking for, today?</h2>
+
+<QuickSearch />
+
+<style>
+	:global(main) {
+		display: flex;
+		flex-direction: column;
+		gap: var(--grid-gap);
+	}
+
+	h2 {
+		text-align: center;
+	}
+</style>
