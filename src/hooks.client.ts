1e6527c301fa805b7c039a35c4ffba20083613ba
kurozenzen <kurozenzen@gmail.com>
2026-07-25 12:43:25 +0200

    feat: Improve sentry reporting

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -7,7 +7,13 @@ import '$lib/logic/firebase/firebase';
 Sentry.init({
 	dsn: 'https://21b348fd524f0146bb18944e30d77b3d@o955708.ingest.us.sentry.io/4509889578270720',
 	tracesSampleRate: 0.1,
-	enableLogs: true
+	enableLogs: true,
+	beforeSendTransaction(event) {
+		if (event.contexts?.trace?.op === 'n_plus_one_api_calls') {
+			return null; // Discards the transaction event entirely
+		}
+		return event;
+	}
 });
 
 export const handleError = handleErrorWithSentry();
--- src/lib/logic/attachments/clearsrc.ts
+++ src/lib/logic/attachments/clearsrc.ts
@@ -2,6 +2,9 @@ import type { Attachment } from 'svelte/attachments';
 
 export const clearsrc: Attachment = (node) => () => {
 	if (node instanceof HTMLVideoElement) {
+		if (!node.paused) {
+			node.pause();
+		}
 		node.src = '';
 		node.load();
 	}
